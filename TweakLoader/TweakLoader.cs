f34c7dfc533772d7bdb6d6e0477d2bc43deb1607
Pezeee <pezeee001@gmail.com>
2026-02-10 20:57:49 +0400

    Merge Patcher and TweakLoader into a single assembly

--- TweakLoader/TweakLoader.cs
+++ TweakLoader/TweakLoader.cs
@@ -1,14 +1,71 @@
-using System;
+﻿using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using HarmonyLib;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
 
 namespace Tweaks
 {
     public class TweakLoader
     {
-        public static void Inject()
+        static void Main()
+        {
+            try
+            {
+                Patch();
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Error during patching: {ex.ToString()}");
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static void Patch()
+        {
+            var executable = @"Terraria.exe";
+            var patched = @"Terraria-patched.exe";
+            if (!File.Exists(executable))
+            {
+                throw new Exception($"Failed to read executable: {executable}");
+            }
+
+            File.Copy(executable, $"{executable}.bk", true);
+
+            using (var module = ModuleDefinition.ReadModule(executable))
+            {
+                var method = module.GetType("Terraria.Program").Methods.First((MethodDefinition def) => def.Name == "StartForceLoad");
+
+                var loadMethod = typeof(TweakLoader).GetMethod("Load");
+
+                var firstInstruction = method.Body.Instructions[0];
+                if (firstInstruction.OpCode == OpCodes.Call)
+                {
+                    Console.WriteLine($"Already patched: {executable}");
+                    return;
+                }
+
+                var il = method.Body.GetILProcessor();
+
+                var call = il.Create(
+                    OpCodes.Call,
+                    module.ImportReference(loadMethod)
+                );
+
+                il.InsertBefore(firstInstruction, call);
+
+                module.Write(patched);
+            }
+
+            File.Delete(executable);
+            File.Move(patched, executable);
+
+            Console.WriteLine($"Succesfully patched {executable}");
+        }
+
+        public static void Load()
         {
             string tweaksPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Tweaks");
 
@@ -27,10 +84,9 @@ namespace Tweaks
                     Assembly assembly = Assembly.LoadFrom(dllFile);
                     FileLog.Debug($"Loaded: {assembly.FullName}");
                     Type type = assembly.GetType("Tweaks.Patch");
-                    object instance = Activator.CreateInstance(type);
                     MethodInfo execute = type.GetMethod("Execute");
 
-                    execute.Invoke(instance, null);
+                    execute.Invoke(null, null);
                     FileLog.Debug($"Executed: {assembly.FullName}");
                 }
                 catch (Exception ex)
